f37aeab65ac9e38abd29bd2bf3f5ae8106cc7e67
Driann's <114148680+Driannm@users.noreply.github.com>
2023-11-24 15:09:49 +0700

    Add files via upload

---
+++ Pertemuan Ke-2/Statement If Else/Statement If Else/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* 
+* Created November 24 2023 | 01 : 01
+* By Driann
+*/
+namespace Statement_If_Else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String harga;
+            Console.Write("Masukan Harga Buku : ");
+            harga = Console.ReadLine();
+            int hargabuku = Convert.ToInt32(harga);
+            if (hargabuku < 25000)
+            {
+                Console.Write("Jadi beli buku");
+            }
+            else
+            {
+                Console.Write("Mahal banget sih, ga jadi beli deh");
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ Pertemuan Ke-2/Statement Switch/Statement Switch/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* 
+* Created November 24 2023 | 01 : 07
+* By Driann
+*/
+namespace Statement_Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String harga;
+            Console.Write("Masukan Harga Buku : ");
+            harga = Console.ReadLine();
+            int hargabuku = Convert.ToInt32(harga);
+            if (hargabuku < 25000)
+            {
+                Console.Write("Beli buku aah");
+            }
+            else
+            {
+                Console.Write("Mahal banget sih, ga jadi beli deh");
+            }
+            Console.Read();
+        }
+    }
+}
