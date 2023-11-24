3bdde240f3eeec90430a74e6fc63df106c8bffd5
Driann's <114148680+Driannm@users.noreply.github.com>
2023-11-24 15:11:55 +0700

    Add files via upload

---
+++ Pertemuan Ke-2/Loop For/For Loop/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 
+* Created November 23 2023 | 05 : 22
+* By Driann
+*/
+
+namespace For_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Seluruh Point Yang Anda Kumpulkan");
+            for (int Point = 1; Point <= 10; Point++)
+            {
+                Console.WriteLine("Point Ke :" + Point.ToString());
+            }
+            Console.Read();
+        }
+    }
+}
+
+/*
+ * Pembahasan :
+ * Console.WriteLine        = digunakan untuk mencetak karakter disertai dengan pindah baris.
+ * for (int Point = 1; Point <= 10; Point++) = merupakan sintak penulisan perulangan 
+ * for,dimana nilai awal point diberikan 1 dengan tipe data integer, kemudian diberikan kondisi jika kurang dari atau sama dengan sepuluh, maka point bertambah 1 (++).
+ * { Console.WriteLine ("Point Ke :"+Point.ToString()); = Statement yang akan diulang.}
+ * + Point.ToString()       = digunakan untuk merubah tipe data int pada point kedalam string.
+ */
---
+++ Pertemuan Ke-2/Loop While/While Loop/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* 
+* Created November 24 2023 | 12 : 41
+* By Driann
+*/
+namespace While_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==========Belajar Perulangan Dengan While==========");
+            int loop = 1;
+            while (loop <= 4)
+            {
+                Console.WriteLine("\n Ulang lagi sampai lulus" + loop);
+                loop++;
+            }
+            Console.Read();
+
+        }
+    }
+}
+/* Pembahasan
+ * 
+ * int loop = 1;                        : mendefinisikan tipe data integer pada kata loop dengan diberikan nilai 1.
+ * 
+ * while (loop <= 4)
+ * { Console.WriteLine("\n ulang lagi   : ilustrasinya sintak diatas menjelaskan seandainya nilai pada loop kurang dari / sama dengan 4, 
+ * maka akan mencetak tulisan "Ulang lagi sampai lulus" sebanyak 4 kali.
+ * 
+ * loop++;                              : loop++ ini merupakan nilai loop yang akan bertambah terus menerus sampai kondisi / syarat pada while terpenuhi
+*/
