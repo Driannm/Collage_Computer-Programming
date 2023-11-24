76cfacdd6450c65105bbf6d413f34e4a32d0b968
Driann's <114148680+Driannm@users.noreply.github.com>
2023-11-24 15:13:03 +0700

    Add files via upload

---
+++ Pertemuan Ke-2/Loop Do While/Do While Loop/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Do_While_Loop
+/* 
+* Created November 24 2023 | 12 : 52
+* By Driann
+*/
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Perulangan dengan Do - While");
+        Console.WriteLine("============================");
+        int loop = 1;
+        do
+        {
+            Console.WriteLine("\n Perulangan Yang Ke = " + loop);
+            loop++;
+        }
+        while (loop <= 10);
+        Console.Read();
+    }
+}
+
+/* Pembahasan
+ * int loop = 1; : mendefinisikan tipe data integer pada kata loop dengan diberikan nilai 1.
+ * 
+ * do
+ * { Console.WriteLine("\n Perulangan yang Ke=” + loop); : Perintah ini berarti dijalankan terlebih dahulu sebelum kondisi batas exesuki while (loop <= 10); diberikan.
+ * 
+ * loop++;} : ini merupakan nilai loop yang akan bertambah terus menerus sampai kondisi / syarat pada while terpenuh.
+ */
---
+++ Pertemuan Ke-2/Loop Foreach/Foreach Loop/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* 
+* Created November 24 2023 | 12 : 58
+* By Driann
+*/
+namespace Foreach_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] nama = { "Yuda Andrian", "Alex Hamongan Hasibuan", "Hari Bagus Maulana", "Driann Jaegar" };
+            Console.WriteLine("Perulangan Dengan FOREACH");
+            Console.WriteLine("=========================");
+            foreach (String urutan in nama)
+            {
+                Console.WriteLine("" + urutan);
+            }
+            Console.Read();
+        }
+    }
+}
+
+
