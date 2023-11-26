e5a11d01aef44d28b13264dd333bee6c7336d167
Driann's <114148680+Driannm@users.noreply.github.com>
2023-11-26 20:37:25 +0700

    Add files via upload

---
+++ Pertemuan Ke-4/Aplikasi View Database/Aplikasi View Database/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace Aplikasi_View_Database
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.HotTrack;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(20, 18);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(517, 309);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(195, 364);
+            this.button1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ClientSize = new System.Drawing.Size(558, 459);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Mona Sans SemiBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form1";
+            this.Text = "Aplikasi View Database";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Pertemuan Ke-4/Aplikasi View Database/Aplikasi View Database/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Aplikasi_View_Database
+{
+    public partial class Form1 : Form
+    {
+        string database = ("server = localhost; uid = root; database = sekolah; pwd = '';");
+        public MySqlConnection connect;
+        public MySqlCommand cmd;
+        public MySqlDataAdapter adp;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Connect()
+        {
+            connect = new MySqlConnection(database);
+            connect.Open();
+        }
+
+        public void Disconnect()
+        {
+            connect = new MySqlConnection(database);
+            connect.Close(); 
+        }
+
+        public DataTable View()
+        {
+            string sql = "select * from siswa";
+            DataTable dt = new DataTable();
+            try
+            {
+                Connect();
+                cmd = new MySqlCommand(sql, connect);
+                adp = new MySqlDataAdapter(cmd);
+                cmd.ExecuteNonQuery();
+                adp.Fill(dt);
+                dataGridView1.DataSource = dt;
+            }
+            catch (Exception ali)
+            {
+                MessageBox.Show(ali.Message);
+            }
+            Disconnect();
+            return dt;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            View();
+        }
+    }
+}
