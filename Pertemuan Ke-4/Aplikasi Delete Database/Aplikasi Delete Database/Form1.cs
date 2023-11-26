a737f6d0b28ed9d629efdc82dd26f5f76fd50bde
Driann's <114148680+Driannm@users.noreply.github.com>
2023-11-26 20:39:04 +0700

    Add files via upload

---
+++ Pertemuan Ke-4/Aplikasi Delete Database/Aplikasi Delete Database/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Configuration;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Aplikasi_Delete_Database
+{
+    public partial class Form1 : Form
+    {
+        string database = "server = localhost; database=sekolah; uid=root; pwd=''";
+        public MySqlConnection connect;
+        public MySqlCommand cmd;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void Query()
+        {
+            try
+            {
+                connect = new MySqlConnection(database);
+                connect.Open();
+                string del = "delete from siswa WHERE NO_Induk='" +textBox1.Text+"'";
+                cmd = new MySqlCommand(del, connect);
+                cmd.ExecuteNonQuery();
+                connect.Close();
+                MessageBox.Show("Delete Berhasil!", "informasi", MessageBoxButtons.OK);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Query();
+        }
+    }
+}
---
+++ Pertemuan Ke-4/Aplikasi Update Database/Aplikasi Update Database/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Aplikasi_Update_Database
+{
+    public partial class Form1 : Form
+    {
+        string database = "server = localhost; database=sekolah; uid=root; pwd=''";
+        public MySqlConnection connect;
+        public MySqlCommand cmd;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Query()
+        {
+            try
+            {
+                connect = new MySqlConnection(database);
+                connect.Open();
+                string update = "UPDATE siswa SET Nama='" + textBox2.Text + "'," +
+                    "Jenis_Kelamin ='" + textBox3.Text + "'," +
+                    "Alamat='" + textBox4.Text + "'," +
+                    "Kelas='" + textBox5.Text + "'" +
+                    "WHERE NO_Induk='" + textBox1.Text + "'";
+
+                cmd = new MySqlCommand(update, connect);
+                cmd.ExecuteNonQuery();
+                connect.Close();
+                MessageBox.Show("Update Berhasil!", "Informasi", MessageBoxButtons.OK);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Query();
+        }
+    }
+}
